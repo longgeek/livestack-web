3793f1f069222b6827fb487101e302763d469e4b
Longgeek <longgeek@thstack.com>
2014-06-18 12:32:49 +0800

    Add sendmail func

--- livestack/livestack/apphome/views.py
+++ livestack/livestack/apphome/views.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: Longgeek <longgeek@gmail.com>
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,7 +10,9 @@ from django.views.generic.base import View
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-import models
+from django.template import loader
+from django.core.mail import EmailMultiAlternatives
+import models, re, threading
 
 class Index(View):
     def get(self, request):
@@ -17,4 +23,37 @@ class Index(View):
 class Download(View):
     def post(self, request):
         context = {}
-        return HttpResponse("Checkout your email right now!")
+        email = request.POST.get('email')
+        recontact = re.compile(r'^[a-zA-Z0-9_]{3,18}\@[a-zA-z0-9]{2,10}\.[a-zA-Z0-9]{3,10}(\.[a-zA-Z0-9]{2,10})?$')
+
+        if recontact.match(email):
+            sendmail('this is a django test', email)
+            return HttpResponse("Checkout your email right now!", context)
+        else:
+            return HttpResponse("Insert a valid email address!", context)
+
+
+class EmailThread(threading.Thread):
+    def __init__(self, subject, html_content, email):
+        self.subject = subject
+        self.html_content = html_content
+        self.email = email
+        threading.Thread.__init__(self)
+
+    def run(self):
+        msg = EmailMultiAlternatives(self.subject,
+                                     self.html_content,
+                                     'LiveStack',
+                                     [self.email,])
+        msg.attach_alternative(self.html_content, "text/html")
+        msg.send()
+
+
+def sendmail(subject, email):
+    template_path = "email.html"
+    context = {
+        'email': email.split('@')[0]
+    }
+    html_content = loader.render_to_string(template_path, context)
+    emails = EmailThread(subject, html_content, email)
+    emails.start()
