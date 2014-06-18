740f068bd7d3ad88653da9551090a5faa1d0d21f
Longgeek <longgeek@thstack.com>
2014-06-18 14:43:30 +0800

    Add livestackgroup@thstack.com mail

--- livestack/livestack/apphome/views.py
+++ livestack/livestack/apphome/views.py
@@ -26,8 +26,10 @@ class Download(View):
         email = request.POST.get('email')
         recontact = re.compile(r'^[a-zA-Z0-9_]{3,18}\@[a-zA-z0-9]{2,10}\.[a-zA-Z0-9]{3,10}(\.[a-zA-Z0-9]{2,10})?$')
 
+        who = email
         if recontact.match(email):
-            sendmail('this is a django test', email)
+            sendmail('[LiveStack] Download to LiveStack ISO', [email,])
+            sendmail('[LiveStack] Download to LiveStack ISO', ['livestackgroup@thstack.com',], who)
             return HttpResponse("Checkout your email right now!", context)
         else:
             return HttpResponse("Insert a valid email address!", context)
@@ -44,16 +46,24 @@ class EmailThread(threading.Thread):
         msg = EmailMultiAlternatives(self.subject,
                                      self.html_content,
                                      'LiveStack',
-                                     [self.email,])
+                                     self.email)
         msg.attach_alternative(self.html_content, "text/html")
         msg.send()
 
 
-def sendmail(subject, email):
+def sendmail(subject, email, who=None):
     template_path = "email.html"
-    context = {
-        'email': email.split('@')[0]
-    }
+    for name in email:
+        context = {
+            'email': name.split('@')[0]
+        }
+
+        if name.split('@')[0] == 'livestackgroup':
+            context = {
+                'email': name.split('@')[0],
+                'emailuser': who
+            }
+
     html_content = loader.render_to_string(template_path, context)
     emails = EmailThread(subject, html_content, email)
     emails.start()
