96d868d5cf3257ce156eb15a28837b81d7c3bf9a
Longgeek <longgeek@thstack.com>
2014-06-18 12:33:19 +0800

    Add EMAIL env

--- livestack/livestack/settings.py
+++ livestack/livestack/settings.py
@@ -81,3 +81,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Email setting
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.googlemail.com'
+EMAIL_HOST_USER = 'livestack@thstack.com'
+EMAIL_HOST_PASSWORD = ''
